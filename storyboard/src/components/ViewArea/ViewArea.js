3b3a7de9d1088bc0001d7c5f3fa1ead65fd674ee
Chris <cjmarsh725@gmail.com>
2019-01-24 19:06:42 -0800

    added components for initial layout

---
+++ storyboard/src/components/ViewArea/ViewArea.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ViewArea.css';
+
+class ViewArea extends Component {
+  render() {
+    return (
+      <div className="viewarea-container">
+        ViewArea
+      </div>
+    );
+  }
+}
+
+export default ViewArea;
