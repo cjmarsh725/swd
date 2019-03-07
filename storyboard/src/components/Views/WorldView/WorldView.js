b18f1d3f40848d633c35a9761b866eff8cdad6d2
Chris <cjmarsh725@gmail.com>
2019-03-06 16:48:59 -0800

    created world map and started description

--- storyboard/src/components/Views/WorldView/WorldView.js
+++ storyboard/src/components/Views/WorldView/WorldView.js
@@ -5,7 +5,11 @@ class WorldView extends Component {
   render() {
     return (
       <div className="worldview-container">
-        WorldView
+        <h1>Seranin</h1>
+        <img className="world-map" src="img/seranin-world-map.png" alt="Seranin World Map" />
+        <section className="world-description">
+          <p>Seranin is a world comprised of eleven countries, three on islands of various size and eight more on a single landmass.</p>
+        </section>
       </div>
     );
   }
